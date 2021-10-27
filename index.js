db97eeb32e3fefcbf6dbebb41483754576a3bf0f
nrasel <nrasel452@gmail.com>
2021-10-27 21:37:00 +0600

    added name

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.get('/', (req, res) => {
     res.send('genius car mechanics')
 })
 
+// naimur
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gzjd3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
